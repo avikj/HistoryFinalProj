aad5059cd06909cc09e325e267d906472077518a
avikj <avikjain12345@gmail.com>
2016-04-28 19:25:28 -0700

    Added decryption page

--- js/bin/decryption.js
+++ js/bin/decryption.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-	$('.number-square').css({minHeight: $('.number-square').width()+'px'});
-	alert($('.number-square').width()+' '+$('.number-square').height())
+	//$('.number-square').css({minHeight: $('.number-square').width()+'px'});
+	//alert($('.number-square').width()+' '+$('.number-square').height())
 
 });
