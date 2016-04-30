863311bd8ae980c65b1a5eb9d7a8e577a394b2db
avikj <avikjain12345@gmail.com>
2016-04-29 22:41:58 -0700

    Added functionality to hint button

--- js/bin/decryption.js
+++ js/bin/decryption.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+	$('#hint-button').click(function(){
+		$('#hint-button').addClass('animated bounceOutLeft');
+		$('#hint-button').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			$('#hint-button').html('1=a, 2=b, 3=c...');
+			$('#hint-button').removeClass('animated bounceOutLeft');
+			$('#hint-button').addClass('animated bounceInRight disabled');
+			$('#hint-button').prop('disabled', true);
+			$('#hint-button').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$('#hint-button').removeClass('animated bounceInRight');
+			});
+		});
+	});
+
 	$('form').submit(function(evt){
 		evt.preventDefault();
 		var input = $('#decoded-field').val().toLowerCase().trim()
